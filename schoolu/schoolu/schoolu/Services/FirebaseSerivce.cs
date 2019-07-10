7593a057415571ae6a7cee040500c5e50e7a220f
Anteneh Bedilu <antenehdev@gmail.com>
2019-07-09 22:10:22 -0700

    Adding Services

---
+++ schoolu/schoolu/schoolu/Services/FirebaseSerivce.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using schoolu.Models;
+using System.Linq;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+namespace schoolu.Services
+{
+    class FirebaseSerivce
+    {
+        FirebaseClient firebase = new FirebaseClient("https://antenehdevschoolu.firebaseio.com/");
+
+        public async Task<List<BatchNumber>> GetAllBatchNumbers()
+        {
+            return (await firebase
+            .Child("BatchNumbers")
+            .OnceAsync<BatchNumber>()).Select(item => new BatchNumber
+            {
+                BatchNo = item.Object.BatchNo
+            }).ToList();
+        }
+
+        public async Task AddBatchNumber(string batchNo)
+        {
+            await firebase
+            .Child("BatchNumbers")
+            .PostAsync(new BatchNumber() { BatchNo = batchNo });
+        }
+    }
+}
--- schoolu/schoolu/schoolu/Views/BatchNumberPage.xaml.cs
+++ schoolu/schoolu/schoolu/Views/BatchNumberPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using schoolu.Services;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +13,28 @@ namespace schoolu.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BatchNumberPage : CarouselPage
     {
+        FirebaseSerivce firebaseSerivce = new FirebaseSerivce();
         public BatchNumberPage()
         {
             InitializeComponent();
+        }
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            var allBatchNumbers = await firebaseSerivce.GetAllBatchNumbers();
+            collectionViewBatch.ItemsSource = allBatchNumbers;
+        }
+
+        private async void ButtonAddBatch_Clicked(object sender, EventArgs e)
+        {
+            await buttonAddBatch.TranslateTo(100, 0, 500, Easing.BounceOut);
+            await buttonAddBatch.TranslateTo(0, 0);
+
+            await firebaseSerivce.AddBatchNumber(entryBatch.Text);
+            entryBatch.Text = string.Empty;
+            var allBatchNumbers = await firebaseSerivce.GetAllBatchNumbers();
+            collectionViewBatch.ItemsSource = allBatchNumbers;
+
         }
     }
 }
